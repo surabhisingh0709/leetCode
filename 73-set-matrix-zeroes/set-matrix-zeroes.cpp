055c7a4d68fb2ce30dcc648b75309a28f11a697a
surabhisingh0709 <singhsurabhi0709@gmail.com>
2026-03-14 22:12:26 +0530

    Time: 0 ms (100.00%) | Memory: 21 MB (5.44%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -3,22 +3,33 @@ public:
     
     void setZeroes(vector<vector<int>>& mat)
     {
-       
-       int m = mat.size();
-       int n = mat[0].size();
-       vector<vector<int>> ans = mat;
-       for(int i = 0 ; i < m ; i++)
-       {
-        for(int j=0 ; j<n ;j++)
-        {
-            if(mat[i][j]==0)
-            {
-                for( int p=0 ; p<m ;p++) ans[p][j]=0;
-                for( int q=0 ; q<n ;q++) ans[i][q]=0;
+        int m = mat.size();
+        int n = mat[0].size();
 
+        vector<int> markRow(m,0);
+        vector<int> markCol(n,0);
+
+        //iterating and preparing markrow and markcol arrays
+        // if mark row array is 1 that means all in that row are 0
+        for(int i =0 ; i<m ; i++)
+        {
+            for( int j =0 ;j < n ; j++)
+            {
+                if(mat[i][j]==0)
+                {
+                    markRow[i]=1;
+                    markCol[j]=1;
+                }
             }
-        }
        }
-       mat=ans;
+
+       for(int i =0 ; i<m ; i++)
+        {
+            for( int j =0 ;j < n ; j++)
+            {
+                if(markRow[i]==1 || markCol[j]==1) mat[i][j]=0;
+            }
+       }
+ 
     }
 };

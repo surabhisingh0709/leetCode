1d2f491b8fbbb4bf9825f0466bca618d42e4715c
surabhisingh0709 <singhsurabhi0709@gmail.com>
2026-03-26 10:39:44 +0530

    Time: 11 ms (21.27%) | Memory: 26.9 MB (11.03%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -4,15 +4,14 @@ public:
         int n = nums.size();
         vector<int>ans;
         map<int,int>freq;
+        int minFreq=(n/3)+1;
         for(int i =0 ; i<n ; i++)
         {
-            freq[nums[i]]++;           
-        }
-        for(auto it:freq)
-        {
-            if(it.second > n/3) ans.push_back(it.first);
+            freq[nums[i]]++;  
+            if(freq[nums[i]]==minFreq) ans.push_back(nums[i]);
         }
         return ans;
+        
         
     }
 };

b8b944d2b7b42973f834b2fcfb206d960a1ac418
surabhisingh0709 <singhsurabhi0709@gmail.com>
2026-03-26 11:37:56 +0530

    Time: 0 ms (100.00%) | Memory: 23.6 MB (64.26%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -3,15 +3,40 @@ public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
         vector<int>ans;
-        map<int,int>freq;
-        int minFreq=(n/3)+1;
-        for(int i =0 ; i<n ; i++)
+        int cnt1=0;
+        int cnt2=0;
+        int el1= INT_MIN;
+        int el2= INT_MIN;
+        for(int i =0 ; i<n ;i ++)
         {
-            freq[nums[i]]++;  
-            if(freq[nums[i]]==minFreq) ans.push_back(nums[i]);
+            if(cnt1 ==0 && el2!=nums[i]) {
+                el1=nums[i];
+                cnt1++;
+            }
+            else if(cnt2 ==0 && el1!=nums[i]) {
+                el2=nums[i];
+                cnt2++;
+            }
+            else if(nums[i]==el1)cnt1++;
+            else if(nums[i]==el2)cnt2++;
+            else 
+            {
+                cnt1--;
+                cnt2--;
+            }
+            
+
         }
+        cnt1=0;
+        cnt2=0;
+        for(int i =0 ; i< n ; i++)
+        {
+            if(nums[i]==el1)cnt1++;
+            if(nums[i]==el2)cnt2++;
+        }
+        if(cnt1 > n/3) ans.push_back(el1);
+        if(cnt2 > n/3) ans.push_back(el2);
         return ans;
-        
         
     }
 };

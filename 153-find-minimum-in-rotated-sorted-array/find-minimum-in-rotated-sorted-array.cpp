a27eab0d85d2de195cf0cec3af208ee882309aa3
surabhisingh0709 <singhsurabhi0709@gmail.com>
2026-03-29 11:50:39 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (78.93%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -6,11 +6,7 @@ public:
         int high= n-1;
         int ans=INT_MAX;
 
-        if(nums[0] < nums[n-1]) 
-        {
-            ans= nums[0];
-            return ans;
-        }
+       
 
         while(low<=high)
         {

c886df0f68f79ee87f487580e2d520a354826b3a
surabhisingh0709 <singhsurabhi0709@gmail.com>
2026-04-13 09:45:07 +0530

    Time: 0 ms (100.00%) | Memory: 26.1 MB (68.28%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -3,27 +3,30 @@ public:
     int singleNonDuplicate(vector<int>& nums) {
         int n = nums.size();
 
+        //BETTER
+        int low=1;
+        int high = n-2;
         if(n==1)return nums[0];
+        if(nums[0]!=nums[1]) return nums[0];
+        if(nums[n-1]!=nums[n-2]) return nums[n-1];
 
-        for(int i =0 ; i< n ; i++)
+        while(low<=high)
         {
-            if(i==0 )
-            {
-               if(nums[i]!=nums[i+1]) return nums[i];
-               
-            }
-            else if(i==n-1 )
-            {
-               if(nums[i]!=nums[i-1]) return nums[i];
-              
-            }
-            else 
-            {
-                if(nums[i]!=nums[i+1] && nums[i]!=nums[i-1]) return nums[i];           
-            }
+            int mid= (low+high)/2;
+
+            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]) return nums[mid];
+            //we are on left half
+            else if( (mid%2 == 0 && nums[mid]==nums[mid+1]) || (mid%2 == 1 && nums[mid]==nums[mid-1])) low=mid+1;
+            
+            //we are on right half therefore num on left
+            else high=mid-1;
         }
         return -1;
 
+
+
+
+        //MY APPROACH
         /*int low=0;
         int high=n-1;
         while(low<=high)
@@ -46,5 +49,31 @@ public:
             }
         }
         return -1;*/
+
+
+
+        //BRUTE 
+        /*if(n==1)return nums[0];
+
+        for(int i =0 ; i< n ; i++)
+        {
+            if(i==0 )
+            {
+               if(nums[i]!=nums[i+1]) return nums[i];
+               
+            }
+            else if(i==n-1 )
+            {
+               if(nums[i]!=nums[i-1]) return nums[i];
+              
+            }
+            else 
+            {
+                if(nums[i]!=nums[i+1] && nums[i]!=nums[i-1]) return nums[i];           
+            }
+        }
+        return -1;*/
+
+        
     }
 };

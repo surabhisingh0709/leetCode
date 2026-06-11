141f64766d8766bdd198c268865c3aad26cce030
surabhisingh0709 <singhsurabhi0709@gmail.com>
2026-06-11 12:37:58 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (92.10%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -7,17 +7,16 @@ public:
         if(n!=m) return false;
 
         int a1[26] ={0};
-        int a2[26] ={0};
 
         for(int i =0 ; i< n ;i++)
         {
             a1[s[i] - 'a']++;
-            a2[t[i] - 'a']++;
+            a1[t[i] - 'a']--;
         }
 
         for(int i =0 ; i<26 ; i++)
         {
-            if(a1[i] != a2[i]) return false;
+            if(a1[i] != 0) return false;
         }
         return true;
     }

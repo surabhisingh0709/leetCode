d1e5d45eede6f9b0a6d4d60128064d75d11aa277
surabhisingh0709 <singhsurabhi0709@gmail.com>
2026-06-15 14:48:40 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (80.20%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -1,43 +1,48 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-
         int m = matrix.size();
         int n = matrix[0].size();
         vector<int> ans;
 
-        int top = 0;
-        int bottom = m - 1;
-        int left = 0;
-        int right = n - 1;
+        int left = 0 ;
+        int right = n-1;
+        int top = 0 ;
+        int bottom = m-1;
 
-        while(top <= bottom && left <= right)
+        while(left<=right && top<=bottom)
         {
-            // move right
-            for(int i = left; i <= right; i++)
-                ans.push_back(matrix[top][i]);
+            //go right
+            for(int j = left ; j<=right ; j++)
+            {
+                ans.push_back(matrix[top][j]);
+            }
             top++;
 
-            // move down
-            for(int i = top; i <= bottom; i++)
+            //go down
+            for(int i = top ; i <= bottom ; i++)
+            {
                 ans.push_back(matrix[i][right]);
+            }
             right--;
+            if(left<=right && top<=bottom)
+            {
+            //go left
+            for(int j = right ; j>=left ; j--)
+            {
+                ans.push_back(matrix[bottom][j]);
+            }
+            bottom--;
 
-            // move left
-            if(top <= bottom){
-                for(int i = right; i >= left; i--)
-                    ans.push_back(matrix[bottom][i]);
-                bottom--;
+            //go up 
+            for(int i =bottom ; i>=top ; i--)
+            {
+                ans.push_back(matrix[i][left]);
+            }
+            left++;
             }
 
-            // move up
-            if(left <= right){
-                for(int i = bottom; i >= top; i--)
-                    ans.push_back(matrix[i][left]);
-                left++;
-            }
         }
-
         return ans;
     }
 };

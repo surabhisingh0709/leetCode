5538614782741b8aa09876a1a842e463a96626ad
surabhisingh0709 <singhsurabhi0709@gmail.com>
2026-06-22 16:11:03 +0530

    Time: 3 ms (55.07%) | Memory: 10.9 MB (70.01%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,40 +1,38 @@
 class Solution {
 public:
 
-    static bool cmp(pair<char,int> a, pair<char,int> b)
+    static bool cmp(pair<char,int>a , pair<char,int>b)
     {
-        return a.second > b.second;
+        return a.second>b.second;
     }
 
     string frequencySort(string s) {
-
         unordered_map<char,int> freq;
 
-        // Count frequencies
-        for(char ch : s)
+        for(int i =0 ; i< s.length() ; i++)
         {
-            freq[ch]++;
+            freq[s[i]]++;
         }
 
-        // Store in vector of pairs
         vector<pair<char,int>> vec;
 
-        for(auto it : freq)
+        for(auto it:freq)
         {
-            vec.push_back({it.first, it.second});
+            vec.push_back({it.first , it.second});
         }
 
-        // Sort by frequency descending
-        sort(vec.begin(), vec.end(), cmp);
+        sort(vec.begin() , vec.end() , cmp);
 
-        // Build answer
-        string ans = "";
-
-        for(auto p : vec)
+        string ans="";
+        for(auto it:vec)
         {
-            ans.append(p.second, p.first);
+            for(int i =0 ; i< it.second ; i++)
+            {
+                ans += it.first;
+            }
         }
 
         return ans;
+     
     }
 };

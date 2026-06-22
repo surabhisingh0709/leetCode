1ff27dfc8756bbb37c629953f02d473e3aabb73a
surabhisingh0709 <singhsurabhi0709@gmail.com>
2026-06-22 16:55:31 +0530

    Time: 4 ms (37.96%) | Memory: 18.7 MB (5.19%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -7,7 +7,41 @@ public:
     }
 
     string frequencySort(string s) {
+    
+    //--------bucket sort
         unordered_map<char,int> freq;
+        int n = s.length();
+
+        for(int i =0 ; i< s.length() ; i++)
+        {
+            freq[s[i]]++;
+        }
+
+        vector<string>bucket(n+1);
+
+        for(auto it:freq)
+        {
+            char ch = it.first;
+            int f = it.second;
+            bucket[f] += ch;
+        }
+
+        string ans="";
+
+        for(int f = n; f >= 1; f--)
+        {
+            for(char ch : bucket[f])
+            {
+                ans.append(f, ch);
+            }
+        }
+        return ans;
+
+
+
+
+    //--------map + sort
+       /* unordered_map<char,int> freq;
 
         for(int i =0 ; i< s.length() ; i++)
         {
@@ -32,7 +66,7 @@ public:
             }
         }
 
-        return ans;
+        return ans;*/
      
     }
 };

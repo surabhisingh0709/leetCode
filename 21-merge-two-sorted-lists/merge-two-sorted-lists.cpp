7210c5053dbc12ed618c908208b4361e3f2060dc
surabhisingh0709 <singhsurabhi0709@gmail.com>
2026-07-09 11:55:45 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (87.65%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -11,8 +11,30 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+//Optimal
+    
+    if(list1==nullptr && list2 ==nullptr) return list1;
+    if(list1!= nullptr && list2 ==nullptr) return list1;
+    if(list2!= nullptr && list1 ==nullptr) return list2;
+
+    if(list1->val >list2->val)swap(list1,list2);
+    ListNode* res = list1;
+
+    while(list1!=nullptr && list2!=nullptr)
+    {
+        ListNode* temp = nullptr;
+        while(list1!=nullptr && list1->val <= list2 ->val)
+        {
+            temp =list1;
+            list1 = list1->next;
+        }
+        temp->next = list2;
+        swap(list1 , list2);
+    }
+    return res;
+
 //Brute
-    ListNode* temp1 = list1;
+   /* ListNode* temp1 = list1;
     ListNode* temp2 = list2;
 
     if(list1==nullptr && list2 ==nullptr) return list1;
@@ -75,12 +97,7 @@ public:
         }
     }
 
-    return head;
-
-
-
-
-
+    return head;*/
 
         
     }

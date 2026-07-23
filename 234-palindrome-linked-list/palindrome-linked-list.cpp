f5d7228d4c696f1fdbb9a3276238c7837649ac4c
surabhisingh0709 <singhsurabhi0709@gmail.com>
2026-07-23 10:35:33 +0530

    Time: 4 ms (49.44%) | Memory: 121.8 MB (45.89%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -10,10 +10,49 @@
  */
 class Solution {
 public:
+    ListNode* reverse(ListNode*temp)
+    {
+        if(temp==nullptr || temp->next ==nullptr)return temp;
+        ListNode*prev=nullptr;
+        ListNode*front = temp;
+        while(temp)
+        {
+            front=temp->next;
+            temp->next=prev;
+            prev=temp;
+            temp=front;         
+        }
+        return prev;
+    }
+
+public:
     bool isPalindrome(ListNode* head) {
         if(head==nullptr || head->next ==nullptr) return true;//this are always palindrome
 
-        stack<int>st;
+    //OPTIMAL
+        ListNode*slow=head;
+        ListNode*fast=head;
+
+        while(fast->next!=nullptr && fast->next->next!=nullptr)
+        {
+            slow=slow->next;
+            fast= fast->next->next;
+        }
+        slow->next=reverse(slow->next);
+
+        ListNode*first=head;
+        ListNode*second=slow->next;
+
+        while(second)
+        {
+            if(first->val != second->val)return false;
+            first= first->next;
+            second= second->next;
+        }
+        return true;
+
+    //BRUTE USING STACK
+        /*stack<int>st;
         ListNode*temp=head;
 
         while(temp)
@@ -29,7 +68,7 @@ public:
             st.pop();
             temp= temp->next;
         }
-        return true;
+        return true;*/
         
         
     }

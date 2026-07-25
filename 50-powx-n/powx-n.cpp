c508dfcbe741559e253b4d49023856714bbf4b9c
surabhisingh0709 <singhsurabhi0709@gmail.com>
2026-07-25 18:49:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (44.07%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -5,8 +5,10 @@ public:
         if(n==0) return 1.0;
         if(n==1) return x;
 
+        if(n%5==0) return Pow(x*x*x*x*x,n/5);
         if(n%2==0) return Pow(x*x,n/2);
         if(n%3==0) return Pow(x*x*x,n/3);
+
 
         return x*Pow(x,n-1);
         
